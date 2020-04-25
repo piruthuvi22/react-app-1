51e0ff9a06fdbc4b2367e4a80195631041bb5ad4
piruthuvi22 <piruthuviraj22@gmail.com>
2020-04-25 21:59:47 +0530

    Updates

---
+++ precache-manifest.59934685176406118fe960d5ce8c7bd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccaf97ff8a228c999775dacdd3e69a32",
+    "url": "/react-app-1/index.html"
+  },
+  {
+    "revision": "3fb4f09c9a585796ec02",
+    "url": "/react-app-1/static/js/2.53fd1613.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-1/static/js/2.53fd1613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994e71dfc1c0b4c45be6",
+    "url": "/react-app-1/static/js/main.2ba89087.chunk.js"
+  },
+  {
+    "revision": "99af2ccb3048941cc0e6",
+    "url": "/react-app-1/static/js/runtime-main.ecdc4ae1.js"
+  },
+  {
+    "revision": "f3786f9af8b8fd09f566e19324d26a1d",
+    "url": "/react-app-1/static/media/img1.f3786f9a.jpeg"
+  },
+  {
+    "revision": "a51a0100400fc66ee47142a11b959891",
+    "url": "/react-app-1/static/media/img2.a51a0100.jpg"
+  },
+  {
+    "revision": "7cc25e65593f6e8fbd98e2dbb15eae3f",
+    "url": "/react-app-1/static/media/img3.7cc25e65.jpg"
+  },
+  {
+    "revision": "44e68d21a2cc2e04c891c3a402317e0e",
+    "url": "/react-app-1/static/media/img4.44e68d21.jpg"
+  }
+]);
